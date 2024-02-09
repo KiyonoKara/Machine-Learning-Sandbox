30ab1c7ffc2b2cca058caee44831e25893ccb140
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2024-02-09 15:35:06 -0500

    Test: Complete unit tests for median

--- test/TestCalculationReference.py
+++ test/TestCalculationReference.py
@@ -1,5 +1,6 @@
 import src.CalculationReference as Cr
 import unittest
+import statistics
 
 
 class TestCalculationReference(unittest.TestCase):
@@ -9,3 +10,10 @@ class TestCalculationReference(unittest.TestCase):
         self.assertEqual(Cr.mean([-2, 2, 4, 8, 20]), 6.4)
         with self.assertRaises(ZeroDivisionError):
             Cr.mean([])
+
+    def test_median(self):
+        self.assertEqual(Cr.median([1, 2, 3, 4, 5]), 3)
+        self.assertEqual(Cr.median([1, 2]), 1.5)
+        self.assertEqual(Cr.median([0]), 0)
+        with self.assertRaises(statistics.StatisticsError):
+            Cr.median([])
